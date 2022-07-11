89a3d1c379f4ba00bfae739f7ba23f528cceb0b4
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-11 13:56:25 +1000

    removed commented TODO lines

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   console.error('putDb not implemented');
 
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
   console.log('data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
   console.error('getDb not implemented');
 
