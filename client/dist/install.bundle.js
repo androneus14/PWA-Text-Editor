4e44ea4a107218b66de4986cb79641e3a4710117
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-11 13:15:47 +1000

    testing to find bugs

--- client/dist/install.bundle.js
+++ client/dist/install.bundle.js
@@ -14,7 +14,6 @@
   !*** ./src/js/install.js ***!
   \***************************/
 /***/ (() => {
-
 eval("const butInstall = document.getElementById('buttonInstall');\r\n\r\n// Logic for installing the PWA\r\n// TODO: Add an event handler to the `beforeinstallprompt` event\r\nwindow.addEventListener('beforeinstallprompt', (event) => {\r\n    // Store the triggered events\r\n    window.deferredPrompt = event;\r\n    // Remove the hidden class from the button.\r\n    butInstall.classList.toggle('hidden', false);\r\n});\r\n\r\n// TODO: Implement a click event handler on the `butInstall` element\r\nbutInstall.addEventListener('click', async () => {\r\n    const promptEvent = window.deferredPrompt;\r\n    if (!promptEvent) {\r\n        return;\r\n    }\r\n    // Show prompt\r\n    promptEvent.prompt();\r\n    // Reset the deferred prompt variable, it can only be used once.\r\n    window.deferredPrompt = null;\r\n    butInstall.classList.toggle('hidden', true);\r\n});\r\n\r\n// TODO: Add an handler for the `appinstalled` event\r\nwindow.addEventListener('appinstalled', (event) => {\r\n    // Clear prompt\r\n    window.deferredPrompt = null;\r\n});\r\n\n\n//# sourceURL=webpack://pwa-text-editor/./src/js/install.js?");
 
 /***/ })
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -27,7 +24,7 @@ module.exports = () => {
       // Injects our custom service worker.
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swDest: 'src-sw.js'
       }),
 
       // Creates a manifest.json file.
@@ -39,7 +36,7 @@ module.exports = () => {
         description: 'Text Editor created through PWAs',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: '/',
+        start_url: './',
         publicPath: './',
         icons: [
           {
